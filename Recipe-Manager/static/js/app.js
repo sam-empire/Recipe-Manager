71e9b106b6c2d5f8833f65c2b41c4b5867beaa30
samuel <samuelsammytwo@gmail.com>
2025-01-29 21:40:16 +0100

    Completed Styling for Recipe pageS

---
+++ Recipe-Manager/static/js/app.js
@@ -0,0 +1,9 @@
+// navbar scripts
+function openNav () {
+    document.getElementById("sideMenu").style.width = "250px";
+}
+
+function closeNav () {
+    document.getElementById("sideMenu").style.width = "0px"
+    
+}
